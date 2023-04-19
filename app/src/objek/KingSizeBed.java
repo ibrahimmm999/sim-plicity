f4c430348ab9ba3027110141609b74ff83855998
estherreginaaa <110697259+estherreginaaa@users.noreply.github.com>
2023-04-19 22:59:01 +0700

    upload objek objek

---
+++ app/src/objek/KingSizeBed.java
@@ -0,0 +1,12 @@
+package objek;
+
+public class KingSizeBed extends Non_Makanan{
+    
+    public KingSizeBed(){
+        super("Kasur King Size", 5, 2, "Tidur", 150);
+    }
+
+    public boolean isOccupied(){
+        return false;
+    }
+}
---
+++ app/src/objek/QueenSizeBed.java
@@ -0,0 +1,11 @@
+package objek;
+
+public class QueenSizeBed extends Non_Makanan {
+    public QueenSizeBed(){
+        super("Kasur Queen Size",4,2,"Tidur",100);
+    }
+
+    public boolean isOccupied(){
+        return false;
+    }
+}
