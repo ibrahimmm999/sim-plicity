897657633e4e7da34ae29b0595de07776af9becc
carissarianda <16521473@mahasiswa.itb.ac.id>
2023-04-27 16:30:42 +0700

    update waktu dan world

--- app/src/main/java/simplicity/World.java
+++ app/src/main/java/simplicity/World.java
@@ -5,27 +5,43 @@ import java.util.ArrayList;
 public class World{
     private int panjang = 0;
     private int lebar = 0;
-    private int time;
-    private int day;
+    private Waktu time;
     private ArrayList<Rumah> listRumah;
 
-    public World(int panjang, int lebar, int time, int day, ArrayList<Rumah> listRumah){
+    public World(int panjang, int lebar, ArrayList<Rumah> listRumah){
         this.panjang = panjang;
         this.lebar = lebar;
-        this.time = time;
-        this.day = day;
         this.listRumah = listRumah;
+        Waktu time = new Waktu();
 
     }
-    public void createSIM();
+    public void createSIM(){
+
+    }
     public int getPanjang(){
         return panjang;
     }
     public int getLebar(){
         return lebar;
     }
-    public int getTime();
-    public int getDay();
+    public String getTime(){
+        return getDay() + " day, " + getHour() + " hour, " + getMinute() + " minute, " + getSecond() + " second";
+    }
+    public int getDay(){
+        return time.getDay();
+    }
+    public int getHour() {
+        return time.getHour();
+    }
+
+    public int getMinute() {
+        return time.getMinute();
+    }
+
+    public int getSecond() {
+        return time.getSecond();
+    }
+
     public ArrayList<Rumah> getListRumah(){
         return this.listRumah;
     }
