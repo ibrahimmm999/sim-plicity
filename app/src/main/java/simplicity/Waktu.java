51b2129afc8de50080c0883fac765ca7ad4aebc2
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-05-01 14:17:27 +0700

    update waktu

--- app/src/main/java/simplicity/Waktu.java
+++ app/src/main/java/simplicity/Waktu.java
@@ -1,68 +1,37 @@
 package simplicity;
 
 public class Waktu {
-    private int day;
-    private int jam;
-    private int menit;
-    private int detik;
-
-    public Waktu(int day, int jam, int menit, int detik) {
-        this.day = day;
-        this.jam = jam;
-        this.menit = menit;
-        this.detik = detik;
-    }
-
-    public Waktu(int jam, int menit, int detik) {
-        this(0, jam, menit, detik);
-    }
-
-    public Waktu(int menit, int detik) {
-        this(0, 0, menit, detik);
-    }
-
-    public Waktu(int sec) {
-        this((int) (sec / (24 * 3600)), (int) (sec / 3600 % 24), (int) (sec / 60 % 60), (int) (sec % 60));
-    }
+    private int hariKe;
+    private int totalDetik; // TOTAL DETIK YANG SUDAH BERJALAN
 
     public Waktu() {
-        this(0, 0, 0, 0);
+        hariKe = 1;
     }
 
-    public int getDay() {
-        return day;
+    public void updateWaktu(int duration) {
+        this.totalDetik += duration;
     }
 
-    public int getHour() {
-        return jam;
+    public int getHari() {
+        hariKe = (totalDetik / 720) + 1;
+        return hariKe;
     }
 
-    public int getMinute() {
-        return menit;
+    public int gettotalDetik() {
+        return totalDetik;
     }
 
-    public int getSecond() {
-        return detik;
+    public int getSisaWaktu() {
+        getHari();
+        return hariKe * 720 - totalDetik;
     }
 
-    public void setDay(int day) {
-        this.day = day;
+    public void wait(int duration) {
+        try {
+            Thread.sleep(duration * 1000);
+        } catch (Exception e) {
+            Thread.interrupted();
+        }
     }
 
-    public void setHour(int jam) {
-        this.jam = jam;
-    }
-
-    public void setMinute(int menit) {
-        this.menit = menit;
-    }
-
-    public void setSecond(int detik) {
-        this.detik = detik;
-    }
-
-    public int convert() {
-        return day * 24 * 60 * 60 + jam * 60 * 60 + menit * 60 + detik;
-    }
-    
 }
